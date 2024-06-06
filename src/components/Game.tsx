382c174676e758fe6bc96ae32f1bd6b6a58def5f
Nicolas Marlier <nmarlier@gmail.com>
2024-06-06 20:19:17 +0200

    +addition, timer

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,28 +2,66 @@ import { useEffect, useState } from "react"
 import NumPad from "./NumPad"
 import './Game.scss'
 
+type GameMode = undefined | '+' | 'x'
+
 const Game = () => {
     const [questions, setQuestions] = useState([] as Question[])
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(-1)
     const [currentAnswer, setCurrentAnswer] = useState(undefined as number | undefined)
+    const [currentGameMode, setCurrentGameMode] = useState(undefined as GameMode)
+    const [startedAt, setStartedAt] = useState(undefined as number | undefined)
+    const [endedAt, setEndedAt] = useState(undefined as number | undefined)
     const [salt, setSalt] = useState(0)
-    const setup = () => {
-        setSalt(getRandomInt(10))
-        setQuestions([...Array(5)].map(() => createRandomQuestion()))   
+    const setup = (gameMode?: GameMode) => {
+        if(gameMode) {
+            setCurrentGameMode(gameMode)
+        }
+        setSalt(salt + 1 + getRandomInt(10))
+        
     }
+
+    useEffect(() => {
+        if(currentGameMode === 'x') {
+            setQuestions([...Array(6)].map(() => createRandomMultiplicationQuestion()))   
+            setStartedAt(Date.now())
+        }
+        else if(currentGameMode === '+') {
+            setQuestions([...Array(6)].map(() => createRandomAdditionQuestion()))   
+            setStartedAt(Date.now())
+        }
+    }, [salt])
+
 
     useEffect(() => {
         setCurrentQuestionIndex(questions.findIndex((q: Question) => q.answer === undefined))
     }, [questions])
+
+    useEffect(() => {
+        if(currentQuestionIndex == -1) {
+            setEndedAt(Date.now())
+        }
+    }, [currentQuestionIndex])
     
-    const createRandomQuestion = (): Question => {
-        const a = getRandomInt(6)
+    const createRandomMultiplicationQuestion = (): Question => {
+        const a = getRandomInt(4) + 2
         const b = getRandomInt(10)
         return {
             a,
             b,
             label: `${a} x ${b} = `,
             correct_answer: a*b,
+            answer: undefined
+        }
+    }
+
+    const createRandomAdditionQuestion = (): Question => {
+        const a = getRandomInt(5) + 1
+        const b = getRandomInt(10)
+        return {
+            a,
+            b,
+            label: `${a} + ${b} = `,
+            correct_answer: a+b,
             answer: undefined
         }
     }
@@ -45,7 +83,7 @@ const Game = () => {
             ]
         )
         setCurrentAnswer(undefined)
-    }
+    }    
 
     const questionStatus = ({answer, correct_answer}: Question, index: number) => {
         if(index == currentQuestionIndex) {
@@ -61,13 +99,14 @@ const Game = () => {
     }
 
     const getSvg = (question: Question) => {
-        return `svg/${question.a * 10 + question.b + 1 + salt}.svg`
+        return `svg/${(question.a * 10 + question.b + 1 + salt) % 150 + 1}.svg`
     }
 
     const answeredQuestions = questions.filter((q: Question) => q.answer !== undefined)
     return <div id='game'>
         { currentQuestionIndex === -1 && questions.length === 0 && <div className='menu'>
-            <div className='button' onClick={setup}>Commencer une partie</div>
+            <div className='button' onClick={() => setup('+')}>TABLES D'ADDITION</div>
+            <div className='button' onClick={() => setup('x')}>TABLES DE MULTIPLICATIONS</div>
         </div>
         }
         { currentQuestionIndex === -1 && questions.length > 0 && <div className='menu'>
@@ -79,8 +118,8 @@ const Game = () => {
                         </div>) }
                 </div>
             </div>
-            <div className='game-result'>{ questions.filter(q => questionStatus(q,0) === 'correct').length} / { questions.length }</div>
-            <div className='button' onClick={setup}>Recommencer une partie</div>
+            <div className='game-result'>{ questions.filter(q => questionStatus(q,0) === 'correct').length} / { questions.length } | {endedAt && startedAt ? Math.round((endedAt - startedAt) / 100) / 10 : '?'}''</div>
+            <div className='button' onClick={() => setup()}>Recommencer une partie</div>
         </div>
         }
         { currentQuestionIndex > -1 && <div className='game-mode'>
